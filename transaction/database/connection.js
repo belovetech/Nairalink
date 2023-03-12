f226b10246ca992027c501b530d22ce73726d2a3
belovetech <belovetech@gmail.com>
2023-03-12 10:54:57 +0100

    [Feat]: structure transaction service

--- transaction/database/connection.js
+++ transaction/database/connection.js
@@ -1,9 +1,16 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('nairalink', 'nairalink', 'Nairalink', {
-  host: '127.0.0.1',
-  dialect: 'mysql',
-});
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/.config.env' });
 
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.HOST,
+    dialect: 'mysql',
+  }
+);
 module.exports = sequelize;
 global.sequelize = sequelize;
--- transaction/server.js
+++ transaction/server.js
@@ -1,6 +1,13 @@
-// const app = require('./app');
+process.on('uncaughtException', (err) => {
+  console.log(`${err.name}: ${err.message}`);
+  console.log('SHUTTING DOWN SOON.............🔥');
+  process.exit(1);
+});
 
-// // const port = process.env.PORT;
-// app.listen(port, () => {
-//   console.log('Listening on port ', port);
-// });
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+app.listen(port, (err) => {
+  if (err) return console.log(`Cannot listen on port ${port}`);
+  console.log(`Listening on port ${port}`);
+});
