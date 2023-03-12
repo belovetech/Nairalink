e2f7286bdb4b3fe97fcdf870fe3127b6cea68cac
slamchillz <mendyslam@gmail.com>
2023-03-12 13:46:29 +0100

    [feature]: Accounts model

---
+++ transaction/models/Account.js
@@ -0,0 +1,80 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+module.exports = sequelize.define('Account', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    unique: true
+  },
+  accountNumber: {
+    type: DataTypes.BIGINT(10),
+    allowNull: false,
+    primaryKey: true,
+    unique: true
+  },
+  firstName: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    validate: {
+      len: {
+        args: [3, 30],
+        msg: "First name should be between 3 and 30 characters"
+      }
+    },
+    set(value) {
+      let newValue = value.trim();
+      newValue = newValue[0].toUpperCase() + newValue.substring(1);
+      this.setDataValue('firstName', newValue);
+    }
+  },
+  lastName: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    validate: {
+      len: {
+        args: [3, 30],
+        msg: "Last name should be between 3 and 30 characters"
+      }
+    },
+    set(value) {
+      let newValue = value.trim();
+      newValue = newValue[0].toUpperCase() + newValue.substring(1);
+      this.setDataValue('lastName', newValue);
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: {
+        msg: 'Email is invalid'
+      }
+    }
+  },
+  currency: {
+    type: DataTypes.STRING,
+    defaultValue: 'NGN',
+    validate: {
+      equals: {
+        args: 'NGN',
+        msg: "NGN is the only acceptable currency"
+      }
+    }
+  },
+  balance: {
+    type: DataTypes.DECIMAL(12, 2),
+    defaultValue: 0.00,
+    allowNull: false,
+    validate: {
+      is: {
+        args: /^[0-9]*\.[0-9]{2}$/i,
+        msg: 'Balance should have a scale of 2'
+      }
+    }
+  }
+}, {
+  freezeTableName: true,
+  timestamps: true
+})
