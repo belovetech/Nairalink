215b4c682333d45a34bb3c30323ae035bd76a2a0
belovetech <belovetech@gmail.com>
2023-03-12 10:57:44 +0100

    [Feat]: structure transaction service

--- transaction/app.js
+++ transaction/app.js
@@ -3,10 +3,6 @@ const router = require('./routes/index');
 
 const app = express();
 
-// DB connection
-// require('./database/connection');
-// require('./controllers/TransactionController');
-
 app.use(express.json());
 app.use(router);
 
