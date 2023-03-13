015786311609e32dc317cc131b7c63cdf356cfa1
belovetech <belovetech@gmail.com>
2023-03-13 10:11:32 +0100

    [Feat]: implement signup

---
+++ controllers/AuthController.js
@@ -0,0 +1,21 @@
+const Customer = require('../models/customerModel');
+
+class AuthController {
+  static async singup(req, res) {
+    const newCustomer = await Customer.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userNamer,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirmation: req.body.passwordConfirmation,
+    });
+
+    return res.status(201).json({
+      status: 'success',
+      newCustomer,
+    });
+  }
+}
+
+module.exports = AuthController;
