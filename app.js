29af66b0b7eebde18c2b5473a1d1750183fe4a20
belovetech <belovetech@gmail.com>
2023-03-13 08:27:20 +0100

    [Feat]: define ednpoint for authentication

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const CustomerRouter = require('./routes/customerRouter');
+const AuthRouter = require('./routes/authRouter');
 
 const app = express();
 
 app.use(express.json());
 
+// ROUTER MIDDLEWARE
+app.use('/api/v1/customers', AuthRouter);
 app.use('/api/v1/customers', CustomerRouter);
 
 module.exports = app;
---
+++ routes/authRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const AuthController = require('../controllers/AuthController');
+
+const router = express.Router();
+
+// AUTHENTICATION
+router.post('/signup', AuthController.signup);
+router.post('/login', AuthController.login);
+router.get('/logout', AuthController.logout);
+
+router.post('/forgetPassword', AuthController.forgetPassword);
+router.post('/resetPassword/:token', AuthController.resetPassword);
+router.patch('/updatePassword', AuthController.updatePassword);
+
+module.exports = router;
