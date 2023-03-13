15e368ecaa145e90732ef6d335da7a1a58d9e372
Hizaram <promisevector@gmail.com>
2023-03-13 20:30:46 +0100

    Changed database name to fix a problem

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -1,5 +1,5 @@
 -- Script to set up the MySQL database
-CREATE DATABASE IF NOT EXISTS card_backend;
+CREATE DATABASE IF NOT EXISTS virtual_cards;
 CREATE USER IF NOT EXISTS 'card_dev'@'localhost' IDENTIFIED BY 'nairalink';
-GRANT ALL PRIVILEGES ON card_backend.* TO 'card_dev'@'localhost';
+GRANT ALL PRIVILEGES ON virtual_cards.* TO 'card_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'card_dev'@'localhost';
