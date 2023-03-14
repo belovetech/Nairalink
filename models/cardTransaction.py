79b242c88ecfa498c58970389d39a6bbda4d4ae3
Hizaram <promisevector@gmail.com>
2023-03-14 14:09:56 +0100

    Added the database schema for card transactions

--- models/cardTransaction.py
+++ models/cardTransaction.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 """Module to define the cardTransaction class"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from datetime import datetime
+from models.card import Base
 
+
+class CardTransaction(Base):
+    """Representation of virtual card transaction model class
+    """
+    __tablename__ = "cardTransactions"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    card_id = Column(Integer, ForeignKey('cards.id'), nullable=False)
+    transaction_type = Column(String(10), nullable=False)
+    description = Column(String(60), nullable=True)
+    currency = Column(String(10), nullable=False)
+    amount = Column(String(15), nullable=False)
+    datetime = Column(DateTime, nullable=False, default=datetime.utcnow)
+    status = Column(String(10), nullable=False)
