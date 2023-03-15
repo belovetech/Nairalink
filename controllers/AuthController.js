4817331efd8fac486a2baf10b33991d4d5de3b56
slamchillz <mendyslam@gmail.com>
2023-03-15 23:15:52 +0100

    [feature]: login and logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -60,6 +60,48 @@ class AuthController {
       });
     }
   }
+
+  static async login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      if (!email || !password) return res.status(400).json({ error: 'Invalid login credentials' });
+      let customer = await Customer.findOne({ email });
+      if (!customer) res.status(404).json({ error: 'Customer not found' });
+      if (customer.isVerified === false) res.status(400).json({ error: 'Kindly verify your email, and come back to login' });
+      customer = await Customer.findOne({ email, password: sha1(password) });
+      if (!customer) res.status(400).json({ error: 'Invalid login credentials' });
+      const token = AuthController.generateToken(customer._id.toString());
+      await redisClient.set(`auth_${token}`, customer._id.toString(), 60 * 60);
+      res.cookie('token', token, {
+        maxAge: 60 * 60,
+        httpOnly: true,
+        sameSite: 'none',
+      });
+      return res.status(200).send({ token, customer: formatResponse(customer) });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async logout(req, res, next) {
+    try {
+      const { authorization } = req.headers;
+      if (!authorization || !authorization.startsWith('Bearer ')) return res.status(401).json({ error: 'Unauthorised' });
+      const token = authorization.split(' ')[1];
+      if (token === undefined) return res.status(401).json({ error: 'Unauthorised' });
+      const valid = await redisClient.get(`auth_${token}`);
+      if (valid === null) return res.status(403).json({ error: 'Unauthorised' });
+      const user = jwt.verify(token, process.env.JWT_SECRET);
+      if (valid !== user.customerId) return res.status(403).json({ error: 'Forbidden' });
+      await redisClient.del(`auth_${token}`);
+      res.status(200).end();
+    } catch (error) {
+      if (error.message === 'invalid signature') return res.status(401).json({ error: 'Unathorised' });
+      if (error.message === 'jwt malformed') return res.status(500).json({ error: 'Server error...' });
+      console.log(error.message);
+      next(error);
+    }
+  }
 }
 
 module.exports = AuthController;
