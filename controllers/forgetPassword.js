c36865dc076a03f9469bbd672bc6e9f7c4ddc0ac
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-15 05:06:45 -0400

    fix: use redis to cache token

--- controllers/forgetPassword.js
+++ controllers/forgetPassword.js
@@ -2,41 +2,35 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/no-extraneous-dependencies
-const Customer = require('../models/customerModel');
-const AppError = require('../helpers/AppError');
-const Token = require("../models/resetToken");
-const sendEmail = require("../helpers/sendEmail");
-const crypto = require("crypto");
-const bcrypt = require("bcrypt");
+const Customer = require('../models/customerModel')
+const sendEmail = require('../helpers/sendEmail')
+const redisClient = require('../db/redis')
+const crypto = require('crypto')
 
 async function forgetPassword(req, res, next) {
-    try {
-      const { email } = req.body;
-      const customer = await Customer.findOne({ email });
+  console.log('here!')
+  try {
+    const { email } = req.body
+    const customer = await Customer.findOne({ email })
 
-      if (!customer) return new AppError("No user with this email address");
+    if (!customer) return res.status(400).json({ error: "user doesn't exist" })
 
-      const token = await Token.findOne({ customerId: customer._id})
-      if (token)  await token.deleteOne();
+    let token = redisClient.get(customer._id)
+    if (token) await redisClient.del(customer._id)
 
-      const resetToken = crypto.randomBytes(32).toString('hex');
-      const hashedToken = await bcrypt.hash(resetToken, Number(bcryptSalt))
+    const resetToken = crypto.randomBytes(32).toString('hex')
 
-      token =  await new Token({
-        customerId: customer._id,
-        token: hashedToken,
-        createdAt: Date.now()
-      }).save()
+    await redisClient.set(customer._id, resetToken, 3600)
+    token = await redisClient.get(customer._id)
 
-      const link = `${process.env.BASE_URL}/passwordReset?token=${token.token}&id=${customer._id}`;
-      await sendEmail(customer.email,"Password Reset Request",link);
+    const link = `${process.env.BASE_URL}/resetPassword?token=${token}&id=${customer._id}`
+    await sendEmail(customer.email, 'Password Reset Request', link)
 
-      res.send("password reset link sent to your email account");
-      return link;
-
-    } catch (err) {
-      return next(err);
-    }
+    res.send(`password reset link sent to ${customer.email}`)
+    return link
+  } catch (err) {
+    return next(err)
   }
+}
 
-module.exports = forgetPassword;
+module.exports = forgetPassword
