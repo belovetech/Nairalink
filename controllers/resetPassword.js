df631a74994a8922edf80cb075ec4255f89f55cd
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-15 15:29:28 -0400

    feat #8: reset password enpoint created
    Signed-off-by: Adebayo Samuel <eladebayoor@gmail.com>

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -2,35 +2,51 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/no-extraneous-dependencies
-const sha1 = require('sha1');
-const Customer = require('../models/customerModel');
-const Token = require("../models/resetToken");
-const sendEmail = require("../helpers/sendEmail");
+const Customer = require('../models/customerModel')
+const sendEmail = require('../helpers/sendEmail')
+const redisClient = require('../db/redis')
+const sha1 = require('sha1')
 
-async function resetPassword (req, res, next) {
+async function resetPassword(req, res, next) {
   try {
-    const { customerId, token, password } = req.body;
-    const resetToken = await Token.findOne({ customerId, token });
-    if (!resetToken) res.status(400).json({ error: 'Invalid or expired password reset token' })
+    const { id, token, newPassword } = req.body
+
+    if (!token || !id || !newPassword) {
+      return res.status(400).json({
+        error:
+          'Invalid request. Please provide token, user ID, and new password.',
+      })
+    }
+
+    const storedToken = await redisClient.get(id)
+    if (!storedToken || storedToken !== token) {
+      return res.status(400).json({
+        error: 'Invalid or expired password reset token.',
+      })
+    }
 
     await Customer.updateOne(
-      { _id: customerId },
-      { $set: { password: sha1(password)}},
-      { new: true }
+      { _id: id },
+      { $set: { password: sha1(newPassword) } },
+      { new: true },
     )
 
-    const customer = await Customer.findById({ _id: customerId });
-    sendEmail(
+    const customer = await Customer.findById({ _id: id })
+    await sendEmail(
       customer.email,
-      "Password Reset Successfully",
-      { firstname: customer.firstName, lastName: customer.lastName },
-      "../helpers/template/resetPassword.handlebars"
-    );
-    await resetToken.deleteOne();
-    return true;
+      'Password Reset Successfully',
+      { name: customer.firstName },
+      '../helpers/template/resetPassword.handlebars',
+    )
+    await redisClient.del(id)
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Password reset successfully.',
+    })
   } catch (err) {
-    return next(err);
+    return next(err)
   }
 }
 
-module.exports = resetPassword;
+module.exports = resetPassword
