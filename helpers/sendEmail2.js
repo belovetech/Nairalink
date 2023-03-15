2d5cce27d5dc32683871f97f59648947daefc69f
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-15 19:10:23 -0400

    refactor: refactor

---
+++ helpers/sendEmail2.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependenciesconst handlebars = require('handlebars')
+const handlebars = require('handlebars')
+const fs = require('fs')
+const path = require('path')
+const { Queue } = require('bullmq')
+
+const queue = new Queue('mailer', {
+  connection: { host: 'localhost', port: 6379 },
+});
+
+const sendEmail = async (email, subject, payload, template) => {
+  const source = fs.readFileSync(path.join(__dirname, template), 'utf8')
+  const compiledTemplate = handlebars.compile(source)
+  const job = {
+    from: 'Nairalink <support@cloudmendy.tech>',
+    subject,
+    to: email,
+    html: compiledTemplate(payload),
+  };
+
+
+  await queue.add('send-simple', job);
+  console.info(`Enqueued an email sending to ${job.to}`);
+};
+
+module.exports = sendEmail;
