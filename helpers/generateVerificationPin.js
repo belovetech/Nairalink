7c2a3319bc6cb6a887ac0e9a775dbe7a16e381b2
belovetech <belovetech@gmail.com>
2023-03-15 13:21:48 +0100

    [Update]:  verification pin

--- helpers/generateVerificationPin.js
+++ helpers/generateVerificationPin.js
@@ -1,8 +1,7 @@
 const generateVerificationPin = () => {
   const min = 100000;
   const max = 999999;
-  const pin = Math.floor(Math.random() * (max - min + 1)) + min;
-  return pin.toString().split('').join(' ');
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 module.exports = generateVerificationPin;
