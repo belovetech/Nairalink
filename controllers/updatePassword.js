8ea3ea67aa5a283620a16c0e3be7fc732e9aa60c
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-15 17:42:00 -0400

    style: linter requirements

--- controllers/updatePassword.js
+++ controllers/updatePassword.js
@@ -8,47 +8,41 @@ const sha1 = require('sha1')
 
 async function updatePassword(req, res, next) {
   try {
-    const { currentPassword, newPassword } = req.body
-    const customerId = req.customer._id // Assuming the user ID is stored in the request object after successful authentication
+    const { customerId, currentPassword, newPassword } = req.body
+    // const customerId = req.customer._id Assuming the user ID is stored in the request object after successful authentication
 
     if (!currentPassword || !newPassword) {
       return res.status(400).json({
-        error: "current and/or new password missing"
+        error: 'current and/or new password missing',
       })
     }
 
-    const customer = await Customer.findById(customerId);
+    const customer = await Customer.findById(customerId)
 
-    if (!customer) return res.status(400).json({
-      error: "user doesn't exist"
-    })
+    if (!customer)
+      return res.status(400).json({
+        error: "user doesn't exist",
+      })
 
     if (customer.password != currentPassword) {
       return res.status(400).json({
-        error: "Incorrect current password"
+        error: 'Incorrect current password',
       })
     }
 
-    let token = redisClient.get(customer._id)
-    if (token) await redisClient.del(customer._id)
-
-    const resetToken = crypto.randomBytes(32).toString('hex')
-
-    await redisClient.set(customer._id, resetToken, 3600)
-
-    const link = `${process.env.BASE_URL}/resetPassword?token=${resetToken}&id=${customer._id}`
-    await sendEmail(customer.email, 'Password Reset Request', {
-      name: customer.firstName,
-      link: link
-    }, "./template/requestResetPassword.handlebars")
+    await Customer.updateOne(
+      { _id: customerId },
+      { $set: { password: sha1(newPassword) } },
+      { new: true },
+    )
 
     return res.status(200).json({
-      status: "success",
-      message: `password reset link sent to ${customer.email}`
+      status: 'success',
+      message: 'password update successful',
     })
   } catch (err) {
     return next(err)
   }
 }
 
-module.exports = forgetPassword
+module.exports = updatePassword
