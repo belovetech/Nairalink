916822af940b3a7b06aaaa4e0a52419646e136b8
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-15 17:13:40 -0400

    fix: updatePasswrod route exposed
    Signed-off-by: Adebayo Samuel <eladebayoor@gmail.com>

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AuthController = require('../controllers/AuthController');
 const forgetPassword = require('../controllers/forgetPassword');
 const resetPassword = require('../controllers/resetPassword');
+const updatePassword = require('../controllers/updatePassword');
 
 const router = express.Router();
 
@@ -12,6 +13,6 @@ router.post('/signup', AuthController.signup);
 
 router.post('/forgetPassword', forgetPassword);
 router.post('/resetPassword/:token', resetPassword);
-// router.patch('/updatePassword', AuthController.updatePassword);
+router.patch('/updatePassword', updatePassword);
 
 module.exports = router;
