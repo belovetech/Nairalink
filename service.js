9a669312838441b49a40ec9de06d4d19308dab4a
belovetech <belovetech@gmail.com>
2023-03-17 11:53:18 +0100

    [Feat]: notification service

---
+++ service.js
@@ -0,0 +1,10 @@
+const worker = require('./worker');
+
+worker.on('completed', (job) =>
+  console.info(
+    `Completed job ${job.id} successfully, sent email to ${job.data.to}`
+  )
+);
+worker.on('failed', (job, err) =>
+  console.info(`Failed job ${job.id} with ${err}`)
+);
