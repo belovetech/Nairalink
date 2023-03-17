0dc09283167003ea2d294b02d0c7b2253ff4ab32
belovetech <belovetech@gmail.com>
2023-03-17 11:45:30 +0100

    [Refactor]: customer route

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -4,20 +4,20 @@ const AppController = require('../controllers/AppController');
 
 const router = express.Router();
 
+// APP CONTROLLERS
 router.get('/stats', AppController.getStats);
 router.get('/status', AppController.getStatus);
-
-// Customers
-router
-  .route('/:id')
-  .get(CustomerController.getCustomer)
-  .patch(CustomerController.updateCustomer) // only admin
-  .delete(CustomerController.deleteCustomer); // only admin
 
 router
   .route('/')
   .post(CustomerController.createCustomer)
   .get(CustomerController.getAllCustomers);
+
+router
+  .route('/:id')
+  .get(CustomerController.getCustomer)
+  .patch(CustomerController.updateCustomer) // only admin
+  .delete(CustomerController.deleteCustomer); // only admin
 
 router.get('/getMe', CustomerController.getMe, CustomerController.getCustomer);
 router.patch('/updateMe', CustomerController.updateCustomer);
