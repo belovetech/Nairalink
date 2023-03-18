22a875bd826ee157ee03b5964eed3f92883da57d
belovetech <belovetech@gmail.com>
2023-03-18 01:58:27 +0100

    [Feat]: add sendEmailVerification and sendPhoneVerification jobs

---
+++ sendPhoneVerification.js
@@ -0,0 +1,33 @@
+/* eslint-disable consistent-return */
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.config.env' });
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+// eslint-disable-next-line import/no-extraneous-dependencies, import/order
+const client = require('twilio')(accountSid, authToken);
+const { Queue } = require('bullmq');
+
+const queue = new Queue('mailer', {
+  connection: { host: 'localhost', port: 6379 },
+});
+
+async function sendPin(phoneNumber, pin) {
+  try {
+    const job = await client.messages.create({
+      body: `Your Verification token ${pin}`,
+      messagingServiceSid: 'MGe12f0e19d3ad3ced889ce36157ca446f',
+      from: 'Nairalink',
+      to: `+234${phoneNumber.slice(1)}`,
+    });
+
+    await queue.add('send-phoneVerification', job);
+    console.info(`Enqueued an email sending to ${job.to}`);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+module.exports = sendPin;
+
