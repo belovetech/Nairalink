67e759a1b337bf95808e8a78027df88917efa900
belovetech <belovetech@gmail.com>
2023-03-18 14:51:35 +0100

    [Fix]: update send pin job typeError

--- helpers/sendPin.js
+++ helpers/sendPin.js
@@ -14,16 +14,17 @@ const queue = new Queue('mailer', {
 
 async function sendPin(phoneNumber, pin) {
   try {
-    const job = await client.messages.create({
+    const job = {
       body: `Your Verification token ${pin}`,
       messagingServiceSid: 'MGe12f0e19d3ad3ced889ce36157ca446f',
       from: 'Nairalink',
       to: `+234${phoneNumber.slice(1)}`,
-    });
+    };
+    const message = await client.messages.create(job);
 
     await queue.add('send-phoneVerification', job);
     console.info(`Enqueued a verification token sending to ${job.to}`);
-    return job;
+    return message;
   } catch (err) {
     console.log(err);
     return err;
