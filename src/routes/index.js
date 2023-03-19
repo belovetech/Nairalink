8383d956fcdca6606423d455f694f0307857d22b
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-19 04:22:00 -0400

    feat: /account/fund/verify post endpoint created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import createAccount from '../controllers/createAccount';
 import retrieveAccount from '../controllers/retrieveAccount';
+import updateAccount from '../controllers/updateAccount';
 
 const router = Router();
 
 router.post('/accounts', createAccount);
 router.get('/accounts/:userId', retrieveAccount);
+router.post('/account/fund/verify', updateAccount);
 // router.post('/transactions', TransactionController.postTransaction);
 // router.get('/transactions', TransactionController.getTransactions);
 
