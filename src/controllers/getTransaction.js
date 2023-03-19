c6599fbd057c509aa4a1c11e22b830f27bfa1eac
belovetech <belovetech@gmail.com>
2023-03-19 22:46:11 +0100

    [Refactor]: implement api features class, use in get transaction route

--- src/controllers/getTransaction.js
+++ src/controllers/getTransaction.js
@@ -1,35 +1,18 @@
+import Transaction from '../models/Transaction';
+import ApiFeatures from '../utils/ApiFeatures';
 /* eslint-disable no-unused-vars */
 /* eslint-disable comma-dangle */
-import { Op } from 'sequelize';
-import Transaction from '../models/Transaction';
-
-//   where: { amount: { [Op.lte]: 8000 } },
-//   where: { amount: { $lte: 8000 } },
-
+/**
+ * Get transaction using query parameters
+ */
 module.exports = async (req, res, next) => {
-  // Filttering
-  const queryObj = { ...req.query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  //   SORTING
-  let sortBy;
-  if (req.query.sort) {
-    sortBy = req.query.sort;
-  } else {
-    sortBy = 'createdAt';
-  }
-
-  // PAGINATION
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 10;
-  const skip = (page - 1) * limit;
-
   try {
+    const query = new ApiFeatures(req.query);
+    const [skip, limit] = [...query.paginate()];
     const transactions = await Transaction.findAll({
-      where: queryObj,
-      attributes: { exclude: ['updatedAt'] },
-      order: [[sortBy, 'DESC']],
+      where: query.filter(),
+      attributes: query.fields(),
+      order: [[query.sort(), 'DESC']],
       offset: skip,
       limit,
     });
