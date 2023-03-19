6fb6dcc5a9ce600f892d25518db71d7f2c321e47
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-19 00:56:35 -0400

    feat: get accounts/:userId exposed
    Signed-off-by: Adebayo Samuel <eladebayoor@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import createAccount from '../controllers/createAccount';
+import retrieveAccount from '../controllers/retrieveAccount';
 
 const router = Router();
 
 router.post('/accounts', createAccount);
+router.get('/accounts/:userId', retrieveAccount);
 // router.post('/transactions', TransactionController.postTransaction);
 // router.get('/transactions', TransactionController.getTransactions);
 
