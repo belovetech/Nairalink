8c18a9730ab9146f3852fd0362729a1bdb86b9f9
belovetech <belovetech@gmail.com>
2023-03-20 02:43:32 +0100

    [Fix]: eslint errors

--- src/controllers/retrieveAccount.js
+++ src/controllers/retrieveAccount.js
@@ -1,21 +1,21 @@
 /**
  * Retrieves an account for a registered customer
  */
-const Account = require('../models/Account')
+const Account = require('../models/Account');
 
 module.exports = async (req, res, next) => {
   try {
-    const { userId } = req.params
+    const { userId } = req.params;
 
-    const account = await Account.findOne({ userId })
+    const account = await Account.findOne({ userId });
 
     if (!account) {
-      return res.status(404).json({ message: 'Account not found' })
+      return res.status(404).json({ message: 'Account not found' });
     }
 
-    return res.status(200).json(account.toJSON())
+    return res.status(200).json(account.toJSON());
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-}
+};
