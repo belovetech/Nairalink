d97c105e890bdc76d15aad058055ce6b4e5f282c
slamchillz <mendyslam@gmail.com>
2023-03-22 08:25:09 +0100

    [feature]: notification client

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,14 +1,17 @@
-import { Queue } from "bullmq";
+import { Queue } from 'bullmq';
 
+/* eslint-disable import/prefer-default-export */
 export class NotificationClient {
-  queue;
   constructor(opts) {
     this.queue = new Queue('notification', opts);
   }
+
+  /* eslint-disable no-unused-vars */
   async enqueue(jobName, job, retry = undefined) {
     await this.queue.add(jobName, job);
-    console.log(`Enqueued an email sending to ${job.to}`);
+    console.log(`Enqueued an mobile alert to ${job.to}`);
   }
+
   close() {
     return this.queue.close();
   }
