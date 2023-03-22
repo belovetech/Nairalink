944538675f082471e9f5802c49dd8fa9e6c27470
slamchillz <mendyslam@gmail.com>
2023-03-22 08:26:40 +0100

    [feature]: debit and credit functions to notify customers on successful transactions

--- src/utils/alert.js
+++ src/utils/alert.js
@@ -10,7 +10,7 @@ dotenv.config({ path: path.join(__dirname, '/../config/.config.env') });
 
 const alertJob = (alertMsg, alertRecipient) => ({
   body: alertMsg,
-  messagingServiceSid: 'MGe12f0e19d3ad3ced889ce36157ca446f',
+  messagingServiceSid: process.env.TWILIO_MESSAGE_SERVICE_SID,
   from: 'Nairalink',
   to: alertRecipient,
 });
@@ -18,41 +18,39 @@ const alertJob = (alertMsg, alertRecipient) => ({
 export const DebitAlert = async (transaction) => {
   const alertRecipient = `+234${transaction.fromAccount}`;
   let alertMsg = `Debit alert!\nAmount:- #${transaction.amount}\n`;
-  const senderBalance = await Account.findOne({
+  const sender = await Account.findOne({
     attributes: ['balance'],
     where: { accountNumber: transaction.fromAccount },
   });
   if (transaction.transactionType === 'transfer') {
-    alertMsg = `${alertMsg}To:- +234${transaction.toAccount}\nBalance:- #${senderBalance}`;
+    alertMsg = `${alertMsg}To:- ${transaction.toAccount}\nBalance:- #${sender.balance}`;
   } else if (transaction.transactionType === 'withdraw') {
-    alertMsg = `${alertMsg}To:- Your Nairalink debit card\nBalance:- #${senderBalance}`;
+    alertMsg = `${alertMsg}To:- Your Nairalink debit card\nBalance:- #${sender.balance}`;
   }
   const job = alertJob(alertMsg, alertRecipient);
   const notification = new NotificationClient({
     connection: { host: 'localhost', port: 6379 },
   });
   await notification.enqueue('phone-message', job);
-  console.info(`Enqueued a Debit '${transaction.transactionType}' alert to '+234${job.to}'`);
   notification.close();
 };
 
 export const CreditAlert = async (transaction) => {
   const alertRecipient = `+234${transaction.toAccount}`;
   let alertMsg = `Credit alert!\nAmount:- #${transaction.amount}\n`;
-  const recipientBalance = await Account.findOne({
+  const recipient = await Account.findOne({
     attributes: ['balance'],
     where: { accountNumber: transaction.toAccount },
   });
   if (transaction.transactionType === 'transfer') {
-    alertMsg = `${alertMsg}From:- +234${transaction.fromAccount}\nBalance:- #${recipientBalance}`;
+    alertMsg = `${alertMsg}From:- ${transaction.fromAccount}\nBalance:- #${recipient.balance}`;
   } else if (transaction.transactionType === 'fund') {
-    alertMsg = `${alertMsg}From:- You, via paystack\nBalance:- #${recipientBalance}`;
+    alertMsg = `${alertMsg}From:- You, via paystack\nBalance:- #${recipient.balance}`;
   }
   const job = alertJob(alertMsg, alertRecipient);
   const notification = new NotificationClient({
     connection: { host: 'localhost', port: 6379 },
   });
   await notification.enqueue('phone-message', job);
-  console.info(`Enqueued a Credit '${transaction.transactionType}' alert to '+234${job.to}'`);
   notification.close();
 };
