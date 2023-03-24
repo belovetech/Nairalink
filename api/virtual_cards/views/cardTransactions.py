3826fb31ada02ca8748744d417b4bee6ee65aeba
Hizaram <promisevector@gmail.com>
2023-03-24 22:44:20 +0100

    Update: Add new routes for cardTransactions

--- api/virtual_cards/views/cardTransactions.py
+++ api/virtual_cards/views/cardTransactions.py
@@ -4,7 +4,15 @@
 from flask import Flask, jsonify, abort, request
 from sqlalchemy.orm.exc import NoResultFound
 from models.engine.db import DB
+from api.worker.processor import send_transaction_status
+from helpers.fundCard import fund_card
 from api.virtual_cards.views import app_views
+
+from rq import Queue
+from redis import Redis
+
+redis_conn = Redis()
+queue = Queue(connection=redis_conn)
 
 app = Flask(__name__)
 db = DB()
@@ -24,16 +32,40 @@ def fund_card():
             currency = data['currency']
         if "amount" in data:
             amount = data['amount']
-        if "status" in data:
-            status = data['status']
         if "narration" in data:
             narration = data['narration']
         else:
             return jsonify({'error': 'Wrong parameters'}), 400
 
         try:
-            card = db.fund_card(card_id, transaction_type, currency, amount, status, narration)
-            return jsonify({'transaction_type': transaction_type , 'amount': amount, 'message': status})
+            res = fund_card(customer_id, amount)
+            resDict = res.json()
+
+            if resDict['status'] == 'failed':
+                return jsonify({'error': resDict['message']})
+            transaction = db.create_transaction(
+                    id=resDict['data']['transactionId'],
+                    card_id=card_id,
+                    transaction_type=transaction_type,
+                    description=narration,
+                    datetime_created = datetime.now()
+                    datetime_updated = datetime.now()
+                    currency=currency,
+                    amount=amount,
+                    status=resDict['status']
+                    )
+
+            job = {
+                "transactionId": resDict['data']['transactionId'],
+                "status": 'successful'
+            }
+            if not transaction:
+                job['status'] = 'failed'
+
+            job_info = queue.enqueue(send_transaction_status, job)
+            print('Transaction with ID {} has been sent for update'.format(job.get('transactionId')))
+
+            card = db.update_card_transaction(id, datetime_updated=datetime.now)
         except ValueError as err:
                 return jsonify({'error': "Unable to perform transaction"}), 400
 
